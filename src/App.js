a8d95936c5b1b2ed0a654e30c86a408bd9a23eeb
Patrick <cayerp@gmail.com>
2020-09-10 19:08:45 -0400

    webpack setup for react
    webpack, scss, js files setup for a basic react project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 
+//scss loads
+import "App.scss";
+
+//file loads
+import thumb from "./tank-top.png";
+
+
+
 
 const App = () => (
-    <div>
-        <h1>Hello React????</h1>
+    <div className="HelloCont">
+        <h1 className="wow" >Hello React????</h1>
+        <img className="helloImg" src={thumb} alt=""/>
     </div>
 );
 
